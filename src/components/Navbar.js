041119e2586da3b09a7b635d3615c834f6792d4e
Tathagat <tathagat1081@gmail.com>
2022-07-27 14:15:54 +0530

    second commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Identicon from 'identicon.js';
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          EthSwap
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
